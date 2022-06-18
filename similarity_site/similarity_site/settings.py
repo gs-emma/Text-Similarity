5d86c2cdba398690efdca85166e0de8a538a6afa
gs-emma <102449471+gs-emma@users.noreply.github.com>
2022-06-18 01:09:56 -0400

    update settings.py

--- similarity_site/similarity_site/settings.py
+++ similarity_site/similarity_site/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&$55*c6*em@eeg2t(a680y#4ww&0lbp*6!yt*&)=s5y55a*o+4'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
